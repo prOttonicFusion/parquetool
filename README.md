69689932599298fa7b7e91526223aa37d6d9c6f7
Otto Lindblom <otto.lindblom@koherent.io>
2024-02-18 15:32:41 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# parquetool
+
+A command-line utility for previewing & analyzing [Apache Parquet](https://parquet.apache.org/) files
+
+## Installation 
+```sh
+git clone git@github.com:prOttonicFusion/parquetool.git
+cd parquetool
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+```sh
+python parquetool.py --help
+```
---
+++ parquetool.py
@@ -0,0 +1,87 @@
+import argparse
+import pandas as pd
+from pyarrow.parquet import ParquetFile, read_metadata, read_schema
+import pyarrow as pa
+
+
+def parquetToDataFrame(path: str) -> pd.DataFrame:
+    df = pd.read_parquet(path)
+    return df
+
+
+def headCmd(args):
+    pf = ParquetFile(args.filePath)
+    firstRows = next(pf.iter_batches(batch_size=args.row_count))
+    df = pa.Table.from_batches([firstRows]).to_pandas()
+    print(df.head(args.row_count))
+
+
+def tailCmd(args):
+    df = parquetToDataFrame(args.filePath)
+    print(df.tail(args.row_count))
+
+
+def metaCmd(args):
+    metadata = read_metadata(args.filePath)
+    print(metadata)
+
+
+def schemaCmd(args):
+    schema = read_schema(args.filePath)
+    print(schema)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(required=True)
+
+    # Head
+    headCmdParser = subparsers.add_parser(
+        "head", help="Print the start of the Parquet contents"
+    )
+    headCmdParser.add_argument("filePath", help="Path to the Parquet file")
+    headCmdParser.add_argument(
+        "-n",
+        "--number",
+        action="store",
+        dest="row_count",
+        help="Number of rows to show",
+        type=int,
+        default=5,
+    )
+    headCmdParser.set_defaults(func=headCmd)
+
+    # Tail
+    tailCmdParser = subparsers.add_parser(
+        "tail", help="Print the end of the Parquet contents"
+    )
+    tailCmdParser.add_argument("filePath", help="Path to the Parquet file")
+    tailCmdParser.add_argument(
+        "-n",
+        "--number",
+        action="store",
+        dest="row_count",
+        help="Number of rows to show",
+        type=int,
+        default=5,
+    )
+    tailCmdParser.set_defaults(func=tailCmd)
+
+    # Meta
+    metaCmdParser = subparsers.add_parser("meta", help="Print the Parquet metadata")
+    metaCmdParser.add_argument("filePath", help="Path to the Parquet file")
+    metaCmdParser.set_defaults(func=metaCmd)
+
+    # Schema
+    schemaCmdParser = subparsers.add_parser(
+        "schema", help="Print the Parquet table schema"
+    )
+    schemaCmdParser.add_argument("filePath", help="Path to the Parquet file")
+    schemaCmdParser.set_defaults(func=schemaCmd)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
